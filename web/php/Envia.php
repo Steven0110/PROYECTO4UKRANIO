4c53d85839b90a740bd11be3859b8b396190c9c0
Steven0110 <cabello.acosta.gerardo@gmail.com>
2017-12-11 14:28:16 -0600

    Protocolo de comunicacion establecido en PHP

--- web/php/Envia.php
+++ web/php/Envia.php
@@ -1,26 +1,101 @@
 <?php
-$palabra = $_POST['cadena'];
+$palabra = "Hola";
+//$palabra = $_POST['cadena'];
+
+
 //$dir_subida = $dir_subida . basename($_FILES['uploadfile']['name']);
 //IP LAURA: 192.168.43.232
+//IP STEVEN: 192.168.43.107
+//IP BRIAN: 192.168.43.206
+
 $MAX_UDP = 1024;
-$server_ip   = '192.168.43.232';
+$server_laura   = [
+  "ip" => "192.168.43.232",
+  "status" => 1,
+  "min" => 0,
+  "max" => 0
+];
+$server_brian   = [
+  "ip" => "192.168.43.206",
+  "status" => 1,
+  "min" => 0,
+  "max" => 0
+];
+$server_steven   = [
+  "ip" => "192.168.43.107",
+  "status" => 1,
+  "min" => 0,
+  "max" => 0
+];
+$servers = [
+  $server_laura, $server_brian, $server_steven
+];
+
 $server_port = 7201;
 $beat_period = 5;
 $micro_beat_period = 100;
 
-if ($socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)) {
-    socket_sendto($socket, $palabra, strlen($palabra), 0, $server_ip, $server_port);
-    while( 1 ){
-  		if(socket_recv ( $sock , $reply , 1024 , MSG_WAITALL ) === FALSE){
-        $errorcode = socket_last_error();
-        $errormsg = socket_strerror($errorcode);
-        die("Could not receive data: [$errorcode] $errormsg \n");
-      }
-      echo $reply;
-    }
-  //Respuesta JSON
-  echo "{ \"status\" : \"1\"}";
-} else {
+$socket = null;
+if ( ($socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)) == false ) {
   echo "{ \"status\" : \"-1\"}";
+  exit(-1);
+}
+    //Obtiene las PCs que están activas
+$code = "2";
+$pc_no = 0;
+socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array("sec"=>3,"usec"=>0));
+foreach( $servers as $server ){
+  //Pregunta a cada servidor si está encendido:
+  socket_sendto($socket, $code, strlen($code), 0, $server->ip, $server_port);
+  //Recibe su respuesta
+  if(socket_recv ( $sock , $reply , 1024 , MSG_WAITALL ) === FALSE){
+    $server->status = 0;
+  }else{
+    $pc_no++;
+    $server->status = 1;
+  }
+}
+
+  //Pregunta por la cantidad total de archivos server por server hasta que uno le responda
+$code = "3";
+$files = 0;
+foreach( $servers as $server ){
+  socket_sendto($socket, $code, strlen($code), 0, $server->ip, $server_port);
+  //Recibe su respuesta
+  if(socket_recv ( $sock , $reply , 1024 , MSG_WAITALL ) === FALSE){
+  }else{
+    $files = intval($reply);
+  }
+}
+
+//Define los rangos que le tocan a cada computadora
+$files_per_pc = floor( $files / $pc_no );
+foreach( $servers as $server ){
+  if($server->status == 1 ){
+    $server->min = $count * $files_per_pc;
+    $server->max = $count * $files_per_pc + $files_per_pc - 1;
+  }
+}
+//Le envía sus rangos a cada server activo y la palabra
+foreach($servers as $server ){
+  if($server->status == 1 ){
+    $min = "".$server->min
+    $max = "".$server->max
+    socket_sendto($socket, $min, strlen($min), 0, $server_ip, $server_port);
+    usleep($micro_beat_period);
+    socket_sendto($socket, $max, strlen($max), 0, $server_ip, $server_port);
+    usleep($micro_beat_period);
+    socket_sendto($socket, $palabra, strlen($palabra), 0, $server_ip, $server_port);
+    usleep($micro_beat_period);
+  }
+}
+//Espera hasta recibir la respuesta de los servers
+foreach($servers as $server){
+  if($server->status == 1 ){
+    if(socket_recv ( $sock , $reply , 1024 , MSG_WAITALL ) === FALSE){
+    }else{
+      echo $reply."<br/>";
+    }
+  }
 }
 ?>
