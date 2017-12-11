f95128e6533a61c1484c9f9a02a56c2930dbd477
Steven0110 <cabello.acosta.gerardo@gmail.com>
2017-12-11 09:14:50 -0600

    udp.php modificado

--- web/php/udp.php
+++ web/php/udp.php
@@ -1,15 +1,24 @@
 <?php
-$server_ip   = '127.0.0.1';
-$server_port = 7200;
+//$server_ip   = '127.0.0.1';
+/*$dir_subida = $dir_subida . basename($_FILES['uploadfile']['name']);
+$server_ip   = '192.168.43.232';*/
+$server_port = 7201;
+$server_ip   = '192.168.43.232';
 $beat_period = 5;
-$message     = 'PyHB';
-print "Sending heartbeat to IP $server_ip, port $server_portn";
-print "press Ctrl-C to stopn";
+$message     = 'Hola Lau :)';
+//print "Sending heartbeat to IP $server_ip, port $server_portn";
+//print "press Ctrl-C to stopn";
+/*
+if (move_uploaded_file($_FILES['fichero_usuario']['tmp_name'], $fichero_subido)) {
+    echo "El fichero es válido y se subió con éxito.\n";
+} else {
+    echo "¡Posible ataque de subida de ficheros!\n";
+}*/
+
 if ($socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)) {
   while (1) {
     socket_sendto($socket, $message, strlen($message), 0, $server_ip, $server_port);
-    print "Time: " . date("%r") . "n";
-    sleep($beat_period);
+    sleep(5);
   }
 } else {
   print("can't create socketn");
