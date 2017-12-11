eb0be18ae1b3b96421d61be85a4b109ab5bcd930
Steven0110 <cabello.acosta.gerardo@gmail.com>
2017-12-11 17:03:44 -0600

    udp.php ahora envia a las tres PC

--- web/php/udp.php
+++ web/php/udp.php
@@ -1,7 +1,9 @@
 <?php
 //IP LAURA: 192.168.43.232
 $MAX_UDP = 1024;
-$server_ip   = '192.168.43.232';
+$server_ip   = '192.168.43.232'; //Laura
+$server_ip_b   = '192.168.43.206'; //Brian
+$server_ip_s  = '192.168.43.107'; //Steven
 $server_port = 7201;
 $beat_period = 5;
 $micro_beat_period = 100;
@@ -11,9 +13,20 @@ $filename = $_FILES["file"]["name"];
 $filename = "../res/".$filename;
 $message = file_get_contents($_FILES["file"]["tmp_name"]);
 if ($socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)) {
+	//Envía a Laura
   socket_sendto($socket, $filename, strlen($filename), 0, $server_ip, $server_port);
   usleep($micro_beat_period );
   socket_sendto($socket, $message, strlen($message), 0, $server_ip, $server_port);
+
+	//Envía a Brian
+  socket_sendto($socket, $filename, strlen($filename), 0, $server_ip_b, $server_port);
+  usleep($micro_beat_period );
+  socket_sendto($socket, $message, strlen($message), 0, $server_ip_b, $server_port);
+
+	//Envía a Steven
+  socket_sendto($socket, $filename, strlen($filename), 0, $server_ip_s, $server_port);
+  usleep($micro_beat_period );
+  socket_sendto($socket, $message, strlen($message), 0, $server_ip_s, $server_port);
 
   //Respuesta JSON
   echo "{ \"status\" : \"1\"}";
