53d41dc0cbd092702a15704075485ba107b6dd16
david <Steven0110>
2017-12-11 15:38:01 -0600

    Cambios

--- c/principal.cpp
+++ c/principal.cpp
@@ -3,41 +3,54 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
-using namespace std;  
-int main()
-{
+#include <stdio.h>
+using namespace std;
+
+int main(){
   int puerto = 7201;
   SocketDatagrama s(puerto);
+  fstream listaArchivos;
+  string frase;
 
-  //char* extension=".txt";
-
-
-  //ofstream fs("archivo.txt");
-
+  
   while(true){
-  /*
-  s.recibe(r);
-    cout << "Datos recibidos: " << r.obtieneDatos() << endl;
-    fs << r.obtieneDatos();
-    fs.close();*/
-
+  	//listaArchivos.open("Lista de archivos.txt", ios::in);
     char* datos;
     char* nombreArchivo;
     PaqueteDatagrama r(1024);
+
+    //Recibiendo nombre de archivo
     s.recibe(r);
     nombreArchivo = (char *)malloc(1024);
     nombreArchivo= (char*)r.obtieneDatos();
     ofstream fs(nombreArchivo);
-    //cout << "Nombre del archivo: " << r.obtieneDatos() << endl;
-    printf("Nombre del archivo: %s\n", nombreArchivo);
+ 	printf("Nombre del archivo: %s\n", nombreArchivo);
+    
+ 	listaArchivos.open("Lista de archivos.txt", ios::app);
+ 	if(listaArchivos.is_open()){ //Si lista de archivos esta abierto
+ 		//while (! listaArchivos.eof() ) { //Mientras no se llegue al fin del archivo
+          //  getline (listaArchivos,frase);
+           // cout << "Leido: " << frase << endl;
+        //}
+ 		listaArchivos<< nombreArchivo<< '\n';
+ 		listaArchivos.close();
+        //listaArchivos << nombreArchivo;
+        //listaArchivos.close();
+ 	}
+ 	else{
+ 		cout << "Fichero inexistente o faltan permisos para abrirlo" << endl; 
+ 	}
+    //Recibiendo contenido del archivo
     s.recibe(r);
     datos = (char *)malloc(1024);
     datos=(char*)r.obtieneDatos();
-    //cout << "Datos recibidos: " << r.obtieneDatos() << endl;
+
     printf("Datos recibidos: %s\n", datos);
     fs << datos;
     fs.close();
 
+    //listaArchivos.close();
   }
+
 }
 
