a9aca4bfd2e9aed58bcefa70c1db9973bcc29a3a
BriaanSz <Steven0110>
2017-12-11 10:58:59 -0600

    DESCRIPCION DEL CAMBIO

--- web/Envia.php
+++ web/Envia.php
@@ -1,4 +1,5 @@
 <?php
+$palabra = $_POST['cadena'];
 //$dir_subida = $dir_subida . basename($_FILES['uploadfile']['name']);
 //IP LAURA: 192.168.43.232
 $MAX_UDP = 1024;
@@ -6,8 +7,6 @@ $server_ip   = '192.168.43.232';
 $server_port = 7201;
 $beat_period = 5;
 $micro_beat_period = 100;
-$filename     = 'prueba.txt';
-$message     = 'Hola Lau :)';
 //print "Sending heartbeat to IP $server_ip, port $server_portn";
 //print "press Ctrl-C to stopn";
 /*
@@ -19,9 +18,9 @@ if (move_uploaded_file($_FILES['fichero_usuario']['tmp_name'], $fichero_subido))
 
 if ($socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)) {
   while (1) {
-    socket_sendto($socket, $filename, strlen($message), 0, $server_ip, $server_port);
+    socket_sendto($socket, $palabra, 0, $server_ip, $server_port);
     usleep($micro_beat_period );
-    socket_sendto($socket, $message, strlen($message), 0, $server_ip, $server_port);
+    socket_sendto($socket, $palabra, 0, $server_ip, $server_port);
     print("Mensaje enviado...\n");
     sleep($beat_period);
   }
