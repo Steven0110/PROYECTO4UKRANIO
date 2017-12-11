e02558d08259a948e2d94197969bd2ce0ffb407f
Steven0110 <cabello.acosta.gerardo@gmail.com>
2017-12-11 16:44:26 -0600

    Envia.php casi terminado

--- web/php/Envia.php
+++ web/php/Envia.php
@@ -37,6 +37,7 @@ $servers = [
 $server_port = 7201;
 $beat_period = 5;
 $micro_beat_period = 100;
+$array = array();
 if ( ($socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)) == false ) {
   echo "{ \"status\" : \"-1 \"}";
   exit(-1);
@@ -44,24 +45,26 @@ if ( ($socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)) == false ) {
       //Obtiene las PCs que están activas - SERVICIO EN EL PUERTO 7202
   $code = "2";
   $pc_no = 0;
+  $i = 0;
   socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array("sec"=>1,"usec"=>0));
   foreach( $servers as $server ){
     //Pregunta a cada servidor si está encendido:
     socket_sendto($socket, $code, strlen($code), 0, $server["ip"], 7202);
     //Recibe su respuesta
     if(socket_recv ( $socket , $reply , 1024 , MSG_WAITALL ) === FALSE){
-      $server["status"] = 0;
     }else{
       $pc_no++;
+      $servers[$i]["status"] = 1;
       echo "Respondió ".$server["name"]."\n";
-      $server["status"] = 1;
     }
+    $i++;
   }
 
     //Pregunta por la cantidad total de archivos server por server hasta que uno le responda
   //  SERVICIO EN EL PUERTO 7203
   $code = "3";
   $files = 0;
+  $i=0;
   foreach( $servers as $server ){
     socket_sendto($socket, $code, strlen($code), 0, $server["ip"], 7203);
     //Recibe su respuesta
@@ -75,15 +78,18 @@ if ( ($socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)) == false ) {
   //Define los rangos que le tocan a cada computadora
   $count = 0;
   $files_per_pc = floor( $files / $pc_no );
+  $i = 0;
   foreach( $servers as $server ){
     if($server["status"]  == 1 ){
-      $server["min"]  = $count * $files_per_pc;
-      $server["max"]  = $count * $files_per_pc + $files_per_pc - 1;
+      $servers[$i]["min"]  = $count * $files_per_pc;
+      $servers[$i]["max"]  = $count * $files_per_pc + $files_per_pc - 1;
       $count++;
-      echo "A ".$server["name"]." le toca de $min a $max\n";
     }
+    $i++;
   }
   //Le envía sus rangos a cada server activo y la palabra - SERVICIO EN EL PUERTO 7201
+  $i = 0;
+  var_dump($servers);
   foreach($servers as $server ){
     if($server["status"] == 1 ){
       $min = "".$server["min"] ;
@@ -94,6 +100,7 @@ if ( ($socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)) == false ) {
       usleep($micro_beat_period);
       socket_sendto($socket, $palabra, strlen($palabra), 0, $server["ip"], $server_port);
       usleep($micro_beat_period);
+      echo "A ".$server["name"]." le tocó de $min a $max\n";
     }
   }
   //Espera hasta recibir la respuesta de los servers
